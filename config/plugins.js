49c96f2bccfd7f7449279437bcd4af87a0e41324
b.adamiat <b.adamiat@irisaco.com>
2022-10-04 13:07:57 +0330

    initial commit

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+    // enable a plugin that doesn't require any configuration
+    //i18n: true,
+    //bigbluebutton:true
+    bigbluebutton:{
+        enabled: false
+    }
+   
+  
+
+  
+    // disable a plugin
+    // myotherplugin: {
+    //   enabled: false, // plugin installed but disabled
+    // },
+  });
